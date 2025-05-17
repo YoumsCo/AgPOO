be098b617cab824f25127935d78ced0ac4f433b7
YoumsCo <youmsc.co@gmail.com>
2025-05-17 19:52:30 +0100

    modified:   models/dbConnexion.php

--- models/dbConnexion.php
+++ models/dbConnexion.php
@@ -1,23 +1,29 @@
 <?php
 session_start();
 
+use Dotenv\Dotenv;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
 class database
 {
-    private $dsn;
-    private const server = $_ENV["SERVER"];
-    private const dbname = $_ENV["DB_NAME"];
-    private const port = $_ENV["PORT"];
-    private const user = $_ENV["PORT"];
-    private const password = $_ENV["PASSWORD"];
     private $pdo;
 
     function __construct()
     {
-        // $this->dsn = "mysql:host=".self::server.";dbname=".self::dbname.";charset=utf8";
-        $this->dsn = "mysql:host=" . self::server . "; dbname=" . self::dbname . "; port=" . self::port . "; charset=utf8";
+        $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+        $dotenv->load();
+
+        $server = $_ENV["SERVER"];
+        $dbname = $_ENV["DB_NAME"];
+        $port = $_ENV["PORT"];
+        $user = $_ENV["USER"];
+        $password = $_ENV["PASSWORD"];
+
+        $dsn = (string) "mysql:host=" . $server . "; dbname=" . $dbname . "; port=" . $port . "; charset=utf8";
 
         try {
-            $this->pdo = new PDO($this->dsn, self::user, self::password);
+            $this->pdo = new PDO($dsn, $user, $password);
             $this->pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
